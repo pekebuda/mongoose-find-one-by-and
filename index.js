d6dba828d41d52f98d20e2a1e6c4b672b1ec228d
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2016-04-02 13:30:28 +0000

    Implementada doble retorno de la funcion: mediante 'callback' o 'throw'.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+/***************************************************************************
+ * @description
+ * 
+ * @param {Object} arguments[0]: propiedades (pares clave-valor) con que ejecutar 
+ * la busqueda del elemento en MongoDB. 
+ * @param {Array} _.tail(arguments): valores pasados como argumentos al metodo 
+ * de instancia invocado (incluyendo posiblemente un callback exigible por 
+ * dicho metodo)
+ * 
+ * @return {Error || Boolean}: En caso de exito, devuelve via callback true. 
+ * En otro caso, ejecuta dicho callback con el Error generado. 
+ */
 var _               = require('lodash')
 ;
 
@@ -9,29 +21,23 @@ module.exports = function(schema, options){
     const STATIC_NAME = "findOneBy" + _.upperFirst(SEARCH_KEY) + "And" + _.upperFirst(METHOD_NAME);
     schema.statics[STATIC_NAME] = findOneByAnd;
     
-    /***************************************************************************
-     * @description
-     * 
-     * @param {Object} arguments[0]: propiedades (pares clave-valor) con que ejecutar 
-     * la busqueda del elemento en MongoDB. 
-     * @param {Array} _.tail(arguments): valores pasados como argumentos al metodo 
-     * de instancia invocado (incluyendo posiblemente un callback exigible por 
-     * dicho metodo)
-     * 
-     * @return {Error || Boolean}: En caso de exito, devuelve via callback true. 
-     * En otro caso, ejecuta dicho callback con el Error generado. 
-     */
+    
     function findOneByAnd(){
         const SEARCH_VALUE = arguments[0];
         const METHOD_ARGUMENTS = _.tail(arguments);
-        
-        this
-            .findOne()
+        const CALLBACK = _.isFunction(_.last(METHOD_ARGUMENTS))? _.last(METHOD_ARGUMENTS): null;
+        this.findOne()
             .where(SEARCH_KEY).equals(SEARCH_VALUE)
             .exec(function(e, r){
-                    if (e) { return cb(e, null) }
-                    else if (r === null) { return cb(new Error("MismatchError querying from " + STATIC_NAME), null) } 
-                    else { return r[METHOD_NAME].apply(r, METHOD_ARGUMENTS) }
+                    if (e) { 
+                        if (CALLBACK) {return CALLBACK(e, null)} 
+                        else {throw e} 
+                    } else if (r === null) {
+                        if (CALLBACK) {return CALLBACK(new Error("MismatchError querying from " + STATIC_NAME), null) } 
+                        else {throw new Error("MismatchError querying from " + STATIC_NAME)}
+                    } else { 
+                        return r[METHOD_NAME].apply(r, METHOD_ARGUMENTS);
+                    }
                 }
             );
     }
